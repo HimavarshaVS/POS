41f454c160de399a04567ac121cf432768b54fde
Himavarsha Goutham <hgoutham@indocresearch.org>
2022-01-04 03:09:54 -0500

    added env config file

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class ConfigClass(object):
-    # DATABASE_URL = "postgresql://varsha:admin@db:5432/pos"
-    DATABASE_URL = "postgresql://varsha:admin@localhost:5432/data"
+    DATABASE_URL = "postgresql://varsha:admin@db:5432/pos"
+    # DATABASE_URL = "postgresql://varsha:admin@localhost:5432/data"
